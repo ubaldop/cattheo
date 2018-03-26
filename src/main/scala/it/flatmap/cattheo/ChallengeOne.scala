df2ebd7ac481e0f9b1f4276181c4cd1ec043d9bf
Peter Pan <P3trur0@users.noreply.github.com>
2018-03-26 10:53:20 +0200

    implementation comment added.

--- src/main/scala/it/flatmap/cattheo/ChallengeOne.scala
+++ src/main/scala/it/flatmap/cattheo/ChallengeOne.scala
@@ -3,6 +3,6 @@ package it.flatmap.cattheo
 object ChallengeOne {
 
   def identity[A](x: A): A = x
-  //curried composition
+  //curried composition, please do this better. It should return a function, not a curried version here.
   def composition[A, B, C](f: A => B, g: B => C)(x: A): C = g(f(x))
 }
